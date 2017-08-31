d5ae70e083b967f8c4271e73e20b4b12dbddd8c3
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-31 19:02:08 -0400

    node_modules_expect_lib_assert_js_GAVE ME ERROR

--- konami_code.js
+++ konami_code.js
@@ -3,4 +3,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
+var index = 0
+document.addEventListener('keydown' function(element) {
+	const key = parseInt(element.detail || element.which)
+		if (key === code[index]) {
+			index++
+
+		if (index === code.length) {
+			alert("YOU DID IT!")
+		}
+	} else {
+		index = 0
+	}
+})
+
 }
